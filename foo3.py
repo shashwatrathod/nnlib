c4eff2f6b952365a69110579261ba6953fdfbf5c
Shashwat Rathod <shashwatrathod22@gmail.com>
2020-09-10 20:29:21 +0530

    cc

--- foo3.py
+++ foo3.py
@@ -1,5 +1,5 @@
 import numpy as np
-from initializers.initializers import Ones
+from initializers.initializers import Zeros
 from activations.activations import LeakyReLU
 
 def der_relu(inputs):
@@ -9,7 +9,7 @@ def der_relu(inputs):
             if inputs[i][j] > 0:
                 op[i][j] = 1
             else:
-                op[i][j] = 0.2 * inputs[i][j]
+                op[i][j] = 0.01 * inputs[i][j]
     return np.array(op)
 
 def der_mse(output,targets):
@@ -18,28 +18,28 @@ def der_mse(output,targets):
 def get_mse_loss(outputs,targets):
     return 0.5*(1/len(outputs))*np.sum(np.square(targets-outputs))
 
-initializer = Ones()
-activation = LeakyReLU(0.1);
-learning_rate = 0.1
+initializer = Zeros()
+activation = LeakyReLU(0.01)
+learning_rate = 0.5
 inputs = np.array([[x] for x in range(1,33)])
 m_input = max(inputs)[0]
 targets = np.array([[0.4*x[0] + 5*x[0] + 7.3] for x in inputs])
 m_target = max(targets)[0]
 inputs = inputs/m_input
 targets = targets/m_target
-layers_units = np.array([2,2,1])
+layers_units = np.array([4,5,1])
 weights = []
 biases = []
 prev_shape = inputs.shape[1]
 for units in layers_units:
-    weight = initializer(units, prev_shape)
+    weight = np.random.rand(prev_shape,units) * 0.1
     bias = initializer(units)
     prev_shape = units
     weights.append(weight)
     biases.append(bias)
 
 losses = []
-for _ in range(1000):
+for _ in range(200):
     #forward pass
     outputs = []
     prev_output = inputs
@@ -53,12 +53,12 @@ for _ in range(1000):
     weights_updated = []
     biases_updated = []
     for i in reversed(range(len(weights))):
-        if(i==(len(weights)-1)): #it's the output layer
+        if(i==(len(weights)-1)):
             t1 = der_mse(outputs[-1],targets)
             t2 = der_relu(np.dot(outputs[i-1],weights[i])+biases[i])
             t3 = outputs[i-1]
         else:
-            t1 = t2*np.dot(t1,weights[i+1].T)###########################################
+            t1 = np.dot(t1*t2,weights[i+1].T)
             if(i==0):
                 t2 = der_relu(np.dot(inputs,weights[i])+biases[i])
                 t3 = inputs
@@ -67,16 +67,13 @@ for _ in range(1000):
                 t3 = outputs[i-1]
 
         t4 = np.ones(shape=biases[i].shape)
-        gradient_w = np.sum(t1 * t2 * t3, axis=0)
-        gradient_b = np.sum(t1 * t2 * t4, axis=0)
-        weight_u = (weights[i].T - learning_rate * (1 / len(inputs)) * gradient_w).T
+        gradient_w = np.dot(t3.T,(t1*t2))
+        gradient_b = np.sum(t1 * t2, axis=0,keepdims=True)
+        weight_u = (weights[i] - learning_rate * (1 / len(inputs)) * gradient_w)
         bias_u = (biases[i] - learning_rate * (1 / len(inputs)) * gradient_b)
-        if (np.shape(weight_u) != weights[i].shape):
-            weight_u = (weights[i] - learning_rate * (1 / len(inputs)) * gradient_w)
-            print(f"weights[{i}] and updated shape dont match")
 
-        if (np.shape(bias_u) != biases[i].shape):
-            print(f"biases[{i}] and updated shape dont match")
+        assert (weight_u.shape == weights[i].shape)
+        assert (bias_u.shape == biases[i].shape)
         weights_updated.append(weight_u)
         biases_updated.append(bias_u)
 
@@ -85,13 +82,19 @@ for _ in range(1000):
 
 import matplotlib.pyplot as plt
 
+plt.subplot(131)
 plt.plot(list(range(len(losses))), losses)
-plt.show()
 
 # VISUALIZATION
-
+plt.subplot(132)
 plt.plot(inputs,targets)
+
+plt.subplot(133)
+prev_output = inputs
+for i in range(len(weights)):
+    output = activation(np.dot(prev_output, weights[i]) + biases[i])
+    prev_output = output
+plt.plot(inputs,prev_output)
 plt.show()
 
-plt.plot(inputs,outputs[-1])
-plt.show()
+dfwevgegre
